f3e348894164e88902e875e91f5e546c6c7a9771
Steve Winton <stevewinton@gmail.com>
2010-08-16 19:34:53 +0100

    Moved things around into friendly package format

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='iremote',
+    version='0.1',
+    description='',
+    author='Steve Winton',
+    author_email='stevewinton@gmail.com',
+    url='http://github.com/swinton/iremote.py',
+    packages=['iremote'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: MacOS X",
+        "Intended Audience :: Developers",
+        "License :: Free For Home Use",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Topic :: Desktop Environment",
+    ],
+    keywords='apple osx iremoted desktop',
+    license='GPL',
+    install_requires=[
+        'setuptools',
+    ],
+)
